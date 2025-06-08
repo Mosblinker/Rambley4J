2b612139c5a3c4bb255f6090ca806fbc31f5f88f
Mosblinker <msteier2.0@gmail.com>
2025-06-08 14:24:04 -0500

    Logging in main method
    I've improved the logging in the main method.

--- src/raccoon/Rambley4J.java
+++ src/raccoon/Rambley4J.java
@@ -1381,22 +1381,15 @@ public class Rambley4J extends JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Rambley4J.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Rambley4J.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Rambley4J.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Rambley4J.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException | InstantiationException | 
+                IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            getLogger().log(Level.SEVERE, "Failed to load Nimbus LnF", ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Rambley4J(true).setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new Rambley4J(true).setVisible(true);
         });
     }
     
